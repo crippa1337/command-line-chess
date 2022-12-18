ecf0e94fc31b9d2b6f6e2f29d29442459f30318c
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-12-18 23:10:33 +0100

    trying to implement input to move

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use std::io::{stdin, Write};
 use ansi_term::Colour::{Fixed, Red, White, RGB};
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
     board.draw_board();
-    player_input(board);
+    player_input(&mut board);
 }
 
 fn clear_screen() {
     print!("{}[2J", 27 as char);
 }
 
-fn player_input(board: Board) {
+fn player_input(board: &mut Board) {
     'initial: loop {
         print!("{} ", White.bold().paint(">>>"));
         std::io::stdout().flush().unwrap();
@@ -24,11 +24,185 @@ fn player_input(board: Board) {
             std::process::exit(0);
         }
 
-        if input.len() != 5 {
+        if input.len() != 4 {
             input_error(Error::Length);
             continue;
         }
+
+        let (from, to) = match_input(input);
+
+        if from.0 == 99 || from.1 == 99 || to.0 == 99 || to.1 == 99 {
+            input_error(Error::OutOfBounds);
+            continue;
+        }
+
+        if board.tiles[from.1][from.0].piece.piece_type == Type::Empty {
+            input_error(Error::Empty);
+            continue;
+        }
+
+        if board.tiles[from.1][from.0].piece.colour != Colour::White {
+            input_error(Error::Color);
+            continue;
+        }
+
+        board.tiles[to.1][to.0].piece = board.tiles[from.1][from.0].piece;
+        board.tiles[from.1][from.0].piece.piece_type = Type::Empty;
+        board.draw_board();
+        break 'initial;
     }
+}
+
+fn match_input(input: String) -> ((usize, usize), (usize, usize)) {
+    let mut chars = input.chars();
+    let mut from = String::new();
+    let mut to = String::new();
+
+    for _ in 0..2 {
+        from.push(chars.next().unwrap());
+    }
+
+    for _ in 0..2 {
+        to.push(chars.next().unwrap());
+    }
+
+    let from: (usize, usize) = match from.as_str() {
+        "a1" => (0, 0),
+        "a2" => (1, 0),
+        "a3" => (2, 0),
+        "a4" => (3, 0),
+        "a5" => (4, 0),
+        "a6" => (5, 0),
+        "a7" => (6, 0),
+        "a8" => (7, 0),
+        "b1" => (0, 1),
+        "b2" => (1, 1),
+        "b3" => (2, 1),
+        "b4" => (3, 1),
+        "b5" => (4, 1),
+        "b6" => (5, 1),
+        "b7" => (6, 1),
+        "b8" => (7, 1),
+        "c1" => (0, 2),
+        "c2" => (1, 2),
+        "c3" => (2, 2),
+        "c4" => (3, 2),
+        "c5" => (4, 2),
+        "c6" => (5, 2),
+        "c7" => (6, 2),
+        "c8" => (7, 2),
+        "d1" => (0, 3),
+        "d2" => (1, 3),
+        "d3" => (2, 3),
+        "d4" => (3, 3),
+        "d5" => (4, 3),
+        "d6" => (5, 3),
+        "d7" => (6, 3),
+        "d8" => (7, 3),
+        "e1" => (0, 4),
+        "e2" => (1, 4),
+        "e3" => (2, 4),
+        "e4" => (3, 4),
+        "e5" => (4, 4),
+        "e6" => (5, 4),
+        "e7" => (6, 4),
+        "e8" => (7, 4),
+        "f1" => (0, 5),
+        "f2" => (1, 5),
+        "f3" => (2, 5),
+        "f4" => (3, 5),
+        "f5" => (4, 5),
+        "f6" => (5, 5),
+        "f7" => (6, 5),
+        "f8" => (7, 5),
+        "g1" => (0, 6),
+        "g2" => (1, 6),
+        "g3" => (2, 6),
+        "g4" => (3, 6),
+        "g5" => (4, 6),
+        "g6" => (5, 6),
+        "g7" => (6, 6),
+        "g8" => (7, 6),
+        "h1" => (0, 7),
+        "h2" => (1, 7),
+        "h3" => (2, 7),
+        "h4" => (3, 7),
+        "h5" => (4, 7),
+        "h6" => (5, 7),
+        "h7" => (6, 7),
+        "h8" => (7, 7),
+        _ => (99, 99),
+    };
+
+    let to: (usize, usize) = match to.as_str() {
+        "a1" => (0, 0),
+        "a2" => (1, 0),
+        "a3" => (2, 0),
+        "a4" => (3, 0),
+        "a5" => (4, 0),
+        "a6" => (5, 0),
+        "a7" => (6, 0),
+        "a8" => (7, 0),
+        "b1" => (0, 1),
+        "b2" => (1, 1),
+        "b3" => (2, 1),
+        "b4" => (3, 1),
+        "b5" => (4, 1),
+        "b6" => (5, 1),
+        "b7" => (6, 1),
+        "b8" => (7, 1),
+        "c1" => (0, 2),
+        "c2" => (1, 2),
+        "c3" => (2, 2),
+        "c4" => (3, 2),
+        "c5" => (4, 2),
+        "c6" => (5, 2),
+        "c7" => (6, 2),
+        "c8" => (7, 2),
+        "d1" => (0, 3),
+        "d2" => (1, 3),
+        "d3" => (2, 3),
+        "d4" => (3, 3),
+        "d5" => (4, 3),
+        "d6" => (5, 3),
+        "d7" => (6, 3),
+        "d8" => (7, 3),
+        "e1" => (0, 4),
+        "e2" => (1, 4),
+        "e3" => (2, 4),
+        "e4" => (3, 4),
+        "e5" => (4, 4),
+        "e6" => (5, 4),
+        "e7" => (6, 4),
+        "e8" => (7, 4),
+        "f1" => (0, 5),
+        "f2" => (1, 5),
+        "f3" => (2, 5),
+        "f4" => (3, 5),
+        "f5" => (4, 5),
+        "f6" => (5, 5),
+        "f7" => (6, 5),
+        "f8" => (7, 5),
+        "g1" => (0, 6),
+        "g2" => (1, 6),
+        "g3" => (2, 6),
+        "g4" => (3, 6),
+        "g5" => (4, 6),
+        "g6" => (5, 6),
+        "g7" => (6, 6),
+        "g8" => (7, 6),
+        "h1" => (0, 7),
+        "h2" => (1, 7),
+        "h3" => (2, 7),
+        "h4" => (3, 7),
+        "h5" => (4, 7),
+        "h6" => (5, 7),
+        "h7" => (6, 7),
+        "h8" => (7, 7),
+        _ => (99, 99),
+    };
+
+    return (from, to);
 }
 
 fn input_error(error: Error) {
@@ -45,7 +219,11 @@ fn input_error(error: Error) {
         ),
         Error::InvalidMove => println!("{} {}", Red.bold().paint(">>>"), "Invalid move!"),
         Error::OutOfBounds => println!("{} {}", Red.bold().paint(">>>"), "Out of bounds!"),
-        Error::IncorrectTile => println!("{} {}", Red.bold().paint(">>>"), "Incorrect tile!"),
+        Error::Color => println!(
+            "{} {}",
+            Red.bold().paint(">>>"),
+            "You can't move your opponent's piece!"
+        ),
     }
 }
 
@@ -183,7 +361,6 @@ impl Board {
     fn draw_board(&self) {
         let grey = RGB(80, 80, 80);
         let brown = Red;
-        println!("      A   B   C   D   E   F   G   H");
         for (row_id, row) in self.tiles.iter().enumerate() {
             print!(
                 "{}\n {}  ",
@@ -227,6 +404,7 @@ impl Board {
             println!("{}", grey.paint("|"));
         }
         println!("{}", grey.paint("    +---+---+---+---+---+---+---+---+"));
+        println!("      a   b   c   d   e   f   g   h");
     }
 }
 
@@ -264,5 +442,5 @@ enum Error {
     Empty,
     InvalidMove,
     OutOfBounds,
-    IncorrectTile,
+    Color,
 }
